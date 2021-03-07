862b2f6343f51119423ea70642f918d884af192c
MexBanDoc <57266899+MexBanDoc@users.noreply.github.com>
2021-03-07 21:38:51 +0300

    Add files via upload

---
+++ www/testmemes.js
@@ -0,0 +1,29 @@
+function getValue() {
+    let res = document.getElementById('wrapper')
+        .querySelectorAll("#response-form input[name=\"response\"]");
+
+    for (let i = 0; i < res.length; i++) {
+        if (res[i].checked) {
+            return res[i].value;
+        }
+    }
+    return -1;
+}
+
+document.getElementById('noLink1').addEventListener('click', (e) => {
+    e.preventDefault();
+    setTimeout(alert("Еще не реализовано :("), 1000);
+});
+
+document.getElementById('noLink2').addEventListener('click', (e) => {
+    e.preventDefault();
+    setTimeout(alert("Еще не реализовано :("), 1000);
+});
+
+document.getElementById('but2').addEventListener("click", (e) => {
+    let ans = getValue();
+    if (ans === -1) {
+        e.preventDefault();
+        alert("Выберите ответ!")
+    }
+});
